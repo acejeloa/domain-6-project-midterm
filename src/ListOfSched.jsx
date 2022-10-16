d59564a396d20e7c3d928f489a8ae2d0e6f68db4
unknown <chadiegilaugis@gmail.com>
2022-10-16 21:23:48 +0800

    initial

---
+++ src/ListOfSched.jsx
@@ -0,0 +1,30 @@
+function ListOfSched({ schedules }) {
+  return (
+    <div className="col-md-11 overflow-auto">
+      <h1 className="text-info text-center">School Venues</h1>
+      <table className="table table-striped bg-primary">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Course no.</th>
+            <th scope="col">Description</th>
+            <th scope="col">Schedule</th>
+            <th scope="col">Teacher</th>
+          </tr>
+        </thead>
+        <tbody>
+          {schedules?.map((sched, index) => (
+            <tr key={index}>
+              <td>{sched.id}</td>
+              <td>{sched.course_no}</td>
+              <td>{sched.description}</td>
+              <td>{sched.schedule}</td>
+              <td>{sched.teacher}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default ListOfSched;
